f01e1c8ea2db7b1e62c2e1516287bd4baf53c546
Filip Nowicki <filnow3@st.amu.edu.pl>
2023-05-31 13:02:41 +0200

    change tcp to udp

--- python_c#/old_hand.py
+++ python_c#/old_hand.py
@@ -4,7 +4,14 @@ import socket
 import numpy as np
 
 
+
 class HandLandmarkerDetector:
+    '''
+    To run it with unity
+    1. Run the unity game (button start should be blue)
+    2. Run this script
+    3. Wait for 'connected to unity' message
+    '''
     def __init__(self):
         self.mp = mp
         self.cap = None
@@ -13,7 +20,7 @@ class HandLandmarkerDetector:
 
         self.host = "127.0.0.1"
         self.port = 25001
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.base_options = mp.tasks.BaseOptions
         self.hand_landmarker = mp.tasks.vision.HandLandmarker
@@ -21,9 +28,14 @@ class HandLandmarkerDetector:
         self.hand_landmarker_result = mp.tasks.vision.HandLandmarkerResult
         self.vision_running_mode = mp.tasks.vision.RunningMode
 
-        self.data = np.zeros((21, 3), dtype=np.float32)
+        self.x = np.zeros(21, dtype=np.float32)
+        self.y = np.zeros(21, dtype=np.float32)
+        self.z = np.zeros(21, dtype=np.float32)
 
     def run(self, debug: bool = False):
+        '''
+        debug: if True, it will show the camera feed and the point
+        '''
         if self.landmarker is None:
             self._initialize()
 
@@ -35,14 +47,17 @@ class HandLandmarkerDetector:
                 continue
 
             timestamp_ms = self.cap.get(cv2.CAP_PROP_POS_MSEC)
+
             image.flags.writeable = False
             mp_image = self.mp.Image(
                 image_format=self.mp.ImageFormat.SRGB, data=image)
             self.landmarker.detect_async(
                 mp_image, timestamp_ms=int(timestamp_ms))
+            
+            posString = ';'.join([f'{x:.5f},{y:.5f},{z:.5f}' for x, y, z in zip(self.x, self.y, self.z)])
 
-            self.sock.sendto(str.encode(';'.join([f'{x:.5f},{y:.5f},{z:.5f}' for x, y, z in self.data])), (self.host, self.port))
-
+            self.sock.sendall(posString.encode("UTF-8"))
+            
             # NOTE: this is to visualize the point
             if debug:
                 cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
@@ -50,14 +65,15 @@ class HandLandmarkerDetector:
                 break
 
         self.cap.release()
-        cv2.destroyAllWindows()
+        self.cv2.destroyAllWindows()
 
-    def _callback(self, result, output_image: mp.Image, timestamp_ms: int):        
-        if result.hand_landmarks != []:
+    def _callback(self, result, output_image: mp.Image, timestamp_ms: int):
+        if result.hand_landmarks != []:  
             for i in range(21):
-                self.data[i] = [result.hand_landmarks[0][i].x, 
-                                result.hand_landmarks[0][i].y, 
-                                result.hand_landmarks[0][i].z]
+                self.x[i] = result.hand_landmarks[0][i].x
+                self.y[i] = result.hand_landmarks[0][i].y
+                self.z[i] = result.hand_landmarks[0][i].z
+
     def _initialize(self):
         self.cap = cv2.VideoCapture(0)
 
@@ -69,7 +85,8 @@ class HandLandmarkerDetector:
 
         self.landmarker = self.hand_landmarker.create_from_options(
             self.options)
-        
+
+        self.sock.connect((self.host, self.port))
         print('connected to unity')
 
 if __name__ == '__main__':
