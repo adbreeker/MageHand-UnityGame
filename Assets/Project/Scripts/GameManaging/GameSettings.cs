5db33f23638a5704d5de3baaf456a5dd2e7651d3
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2023-11-09 02:14:30 +0100

    Starting mediapipe exe from streamingassets on awake

--- Assets/Project/Scripts/GameManaging/GameSettings.cs
+++ Assets/Project/Scripts/GameManaging/GameSettings.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 
 public class GameSettings : MonoBehaviour
 {
+    static Process mediapipeHandProcess;
+
     //applying player setting, actually hardcoded values
     void Awake()
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
+
+        RunMediapipeExe();
+        Application.quitting += CloseMediapipeExeOnQuit;
     }
 
     private void Start()
@@ -16,5 +23,33 @@ public class GameSettings : MonoBehaviour
         //hide cursor
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void RunMediapipeExe()
+    {
+        if(mediapipeHandProcess == null)
+        {
+            UnityEngine.Debug.Log("Running mediapipe");
+            string handPath = Path.Combine(Application.streamingAssetsPath, "Mediapipe", "hand.exe");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = handPath;
+            startInfo.CreateNoWindow = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            mediapipeHandProcess = new Process();
+            mediapipeHandProcess.StartInfo = startInfo;
+            mediapipeHandProcess.Start();
+        }
+    }
+
+    void CloseMediapipeExeOnQuit()
+    {
+        if (mediapipeHandProcess != null && !mediapipeHandProcess.HasExited) 
+        {
+            UnityEngine.Debug.Log("Killing mediapipe");
+            mediapipeHandProcess.Kill();
+            mediapipeHandProcess.Dispose();
+        }
     }
 }
