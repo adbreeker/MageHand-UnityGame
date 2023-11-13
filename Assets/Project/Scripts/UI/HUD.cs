c72f42fa25d96719bda9367c010973e2445bf3ba
Bilox01 <biloxwolf@gmail.com>
2023-11-13 15:02:51 +0100

    mana bar and fixed dialogue hitbox from spells

--- Assets/Project/Scripts/UI/HUD.cs
+++ Assets/Project/Scripts/UI/HUD.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
     public TextMeshProUGUI gestureText;
-    private string ununnormalizedText;
+    //public TextMeshProUGUI manaText;
+    public GameObject manaBox;
+    public Image manaBar;
+    private string ununnormalizedGestureText;
 
     void Update()
     {
+        ShowGesture();
+        ShowMana();
+    }
+
+    void ShowGesture()
+    {
         if (!PlayerParams.Variables.uiActive || PlayerParams.Controllers.inventory.inventoryOpened)
         {
-            ShowText();
+            ununnormalizedGestureText = GameObject.FindGameObjectWithTag("MainCamera").transform.Find("Hand").GetComponent<MoveHandPoints>().gesture;
+            if (ununnormalizedGestureText == "Pointing_Up") gestureText.text = "Click";
+            else if (ununnormalizedGestureText == "Closed_Fist") gestureText.text = "Grab";
+            else if (ununnormalizedGestureText == "Thumb_Up") gestureText.text = "Throw";
+            else if (ununnormalizedGestureText == "Victory") gestureText.text = "Cast";
+            else if (ununnormalizedGestureText == "Thumb_Down") gestureText.text = "Equip";
+            else if (ununnormalizedGestureText == "ILoveYou") gestureText.text = "Drink";
+            else gestureText.text = "";
         }
         else
         {
@@ -20,15 +37,18 @@ public class HUD : MonoBehaviour
         }
     }
 
-    void ShowText()
+    void ShowMana()
     {
-        ununnormalizedText = GameObject.FindGameObjectWithTag("MainCamera").transform.Find("Hand").GetComponent<MoveHandPoints>().gesture;
-        if (ununnormalizedText == "Pointing_Up") gestureText.text = "Click";
-        else if (ununnormalizedText == "Closed_Fist") gestureText.text = "Grab";
-        else if (ununnormalizedText == "Thumb_Up") gestureText.text = "Throw";
-        else if (ununnormalizedText == "Victory") gestureText.text = "Cast";
-        else if (ununnormalizedText == "Thumb_Down") gestureText.text = "Equip";
-        else if (ununnormalizedText == "ILoveYou") gestureText.text = "Drink";
-        else gestureText.text = "";
+        if (!PlayerParams.Variables.uiActive && PlayerParams.Controllers.spellbook.spells.Count > 0)
+        {
+            manaBox.SetActive(true);
+            manaBar.fillAmount = PlayerParams.Controllers.spellCasting.mana / 100;
+            //manaText.text = ((int)PlayerParams.Controllers.spellCasting.mana).ToString() + "/100";
+        }
+        else
+        {
+            manaBox.SetActive(false);
+            //manaText.text = "";
+        }
     }
 }
