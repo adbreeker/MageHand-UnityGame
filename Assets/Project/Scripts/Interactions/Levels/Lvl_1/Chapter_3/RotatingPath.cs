ce0761a015ab415e2ae2d7711a74a0bd105a19c0
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2023-11-17 03:15:45 +0100

    Working level 1 chapter 3
    still need some changes and bonuses but chapter is playable

---
+++ Assets/Project/Scripts/Interactions/Levels/Lvl_1/Chapter_3/RotatingPath.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPath : MonoBehaviour
+{
+    [SerializeField] GameObject _rotationCube;
+
+    bool rotated = false;
+    Vector3 rotationPos;
+
+    void Start()
+    {
+        rotationPos = new Vector3(_rotationCube.transform.position.x, PlayerParams.Objects.player.transform.position.y, _rotationCube.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerParams.Objects.player.transform.position == rotationPos && !rotated)
+        {
+            PlayerParams.Objects.player.transform.rotation *= Quaternion.Euler(0, 180, 0);
+            rotated = true;
+        }
+        if(rotated && PlayerParams.Objects.player.transform.position != rotationPos)
+        {
+            rotated = false;
+        }
+    }
+}
