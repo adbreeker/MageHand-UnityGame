3a6d191062a215abb7dbb86dcdb236b0c2b80e9f
Bilox01 <biloxwolf@gmail.com>
2024-01-15 14:43:32 +0100

    sounds

--- Assets/Project/Scripts/Interactions/MagicEffects/SpellBehaviors/FireSpellBehavior.cs
+++ Assets/Project/Scripts/Interactions/MagicEffects/SpellBehaviors/FireSpellBehavior.cs
@@ -7,6 +7,22 @@ public class FireSpellBehavior : MonoBehaviour
     [Header("Explosion effect prefab")]
     public GameObject explosionEffectPrefab;
 
+    [Header("Sound distance")]
+    public float minHearingDistance = 10.0f;
+    public float maxHearingDistance = 30.0f;
+
+    private GameObject instantiatedEffect;
+
+    private AudioSource spellRemaining;
+    private AudioSource spellBurst;
+
+    private void Start()
+    {
+        spellRemaining = FindObjectOfType<SoundManager>().CreateAudioSource(SoundManager.Sound.SFX_FireSpellRemaining, gameObject, minHearingDistance, maxHearingDistance);
+        spellRemaining.loop = true;
+        spellRemaining.Play();
+    }
+
     public void OnThrow() //rotate fireball on throw to face good direction
     {
         transform.localRotation = Quaternion.Euler(-90, 0, 0);
@@ -14,6 +30,8 @@ public class FireSpellBehavior : MonoBehaviour
 
     public void OnImpact() //spawn explosion on impact
     {
-        Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        instantiatedEffect = Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        spellBurst = FindObjectOfType<SoundManager>().CreateAudioSource(SoundManager.Sound.SFX_FireSpellBurst, instantiatedEffect, 8f, 30f);
+        spellBurst.Play();
     }
 }
