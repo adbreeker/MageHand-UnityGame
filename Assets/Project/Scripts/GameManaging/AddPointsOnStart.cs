ce52851724b11a2fd51b2113aadd0d5b96bea051
Masky <biloxwolf@gmail.com>
2024-04-19 19:44:25 +0200

    fixed old and added new notes on c1l3

--- Assets/Project/Scripts/GameManaging/AddPointsOnStart.cs
+++ Assets/Project/Scripts/GameManaging/AddPointsOnStart.cs
@@ -6,6 +6,8 @@ public class AddPointsOnStart : MonoBehaviour
 {
     public int maxPlotPointsToAdd = 0;
     public int minPlotPointsToAdd = 0;
+    [TextArea(3, 10)]
+    public string whyAdded = "";
     private void Start()
     {
         PlayerParams.Controllers.pointsManager.maxPlotPoints += maxPlotPointsToAdd;
