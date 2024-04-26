2c9743a667d6c7aadcaf257ab173a69260a0eaeb
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2024-04-26 23:55:15 +0200

    moving paintings choice

--- Assets/Project/Scripts/Interactions/Levels/C2L1/ChooseOnePainting.cs
+++ Assets/Project/Scripts/Interactions/Levels/C2L1/ChooseOnePainting.cs
@@ -10,11 +10,33 @@ public class ChooseOnePainting : MonoBehaviour
 
     public Vector3 tpDestination;
 
+    public OpenBarsPassage passage;
+    public LeverBehavior lever;
+    public GameObject cubeLeverTrigger;
+    
+    bool leverTriggered = false;
+
     private void Awake()
     {
         PlayerParams.Controllers.pointsManager.maxPlotPoints += 2;
         PlayerParams.Controllers.pointsManager.minPlotPoints += -2;
     }
+
+    private void Update()
+    {
+        if(!leverTriggered)
+        {
+            Vector3 cubePosWithPlayer = cubeLeverTrigger.transform.position;
+            cubePosWithPlayer.y = 1;
+
+            if(cubePosWithPlayer == PlayerParams.Controllers.playerMovement.currentTilePos)
+            {
+                leverTriggered = true;
+                lever.OnClick();
+            }
+        }
+    }
+
     public void ChosenPaintingInteraction(GameObject painting)
     {
         if(painting == painting1)
@@ -32,5 +54,7 @@ public class ChooseOnePainting : MonoBehaviour
             PlayerParams.Controllers.pointsManager.plotPoints += 2;
             PlayerParams.Controllers.playerMovement.TeleportTo(tpDestination, null);
         }
+
+        passage.Interaction();
     }
 }
