ee4c207c25dfc1be867b551c50e8cb91877c0bb5
Masky <biloxwolf@gmail.com>
2024-08-07 21:14:19 +0200

    new look for wolf and skull platform

--- Assets/Project/Scripts/Interactions/DungeonElements/SkullPlatformBehavior.cs
+++ Assets/Project/Scripts/Interactions/DungeonElements/SkullPlatformBehavior.cs
@@ -11,6 +11,7 @@ public class SkullPlatformBehavior : MonoBehaviour
     [SerializeField] OpenDialogue _dialogue;
 
     [Header("Materials:")]
+    [SerializeField] Renderer _model;
     [SerializeField] Material _platformActiveMat;
     [SerializeField] Material _platformInactiveMat;
 
@@ -30,7 +31,7 @@ public class SkullPlatformBehavior : MonoBehaviour
     IEnumerator PlatformEffect()
     {
         GameObject effect = Instantiate(_platformEffectPrefab, transform);
-        GetComponent<Renderer>().material = _platformInactiveMat;
+        _model.material = _platformInactiveMat;
 
         Vector3 startingScale = effect.transform.localScale;
 
@@ -64,7 +65,7 @@ public class SkullPlatformBehavior : MonoBehaviour
 
     private void OnValidate()
     {
-        if(platformActive) { GetComponent<Renderer>().material = _platformActiveMat;}
-        else { GetComponent<Renderer>().material = _platformInactiveMat; }
+        if(platformActive) { _model.material = _platformActiveMat;}
+        else { _model.material = _platformInactiveMat; }
     }
 }
