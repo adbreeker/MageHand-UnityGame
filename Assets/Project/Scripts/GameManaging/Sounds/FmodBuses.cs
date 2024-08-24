3079b540a61884a05a55fff546c162d402836fae
Masky <biloxwolf@gmail.com>
2024-08-24 22:13:55 +0200

    small names and order changes

--- Assets/Project/Scripts/GameManaging/Sounds/FmodBuses.cs
+++ Assets/Project/Scripts/GameManaging/Sounds/FmodBuses.cs
@@ -8,6 +8,6 @@ public static class FmodBuses
 {
     public static Bus Master => RuntimeManager.GetBus("bus:/");
     public static Bus Music => RuntimeManager.GetBus("bus:/Music");
-    public static Bus Nonpausable => RuntimeManager.GetBus("bus:/Nonpausable");
     public static Bus SFX => RuntimeManager.GetBus("bus:/SFX");
+    public static Bus Nonpausable => RuntimeManager.GetBus("bus:/Nonpausable");
 }
