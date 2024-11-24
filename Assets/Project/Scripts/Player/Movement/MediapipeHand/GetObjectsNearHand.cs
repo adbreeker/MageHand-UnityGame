a3fb8f992d699b5d2b6f817b450df0d27d93ce78
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2024-11-24 15:28:41 +0100

    fixing build preventing bugs

--- Assets/Project/Scripts/Player/Movement/MediapipeHand/GetObjectsNearHand.cs
+++ Assets/Project/Scripts/Player/Movement/MediapipeHand/GetObjectsNearHand.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class GetObjectsNearHand : MonoBehaviour
 {
