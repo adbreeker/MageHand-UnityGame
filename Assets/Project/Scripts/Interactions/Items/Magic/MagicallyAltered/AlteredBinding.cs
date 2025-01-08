decd83715c92aafb672f83955496b250a8b0f2d9
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-01-08 15:52:17 +0100

    Rework of cursed items
    Magicall alter instead of curses
    + knife binded and knife spectral

--- Assets/Project/Scripts/Interactions/Items/Magic/MagicallyAltered/AlteredBinding.cs
+++ Assets/Project/Scripts/Interactions/Items/Magic/MagicallyAltered/AlteredBinding.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursePlaceBind : MonoBehaviour
+public class AlteredBinding : MonoBehaviour
 {
     Vector3 _startingPos;
     Quaternion _startingRot;
