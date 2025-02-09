54b9920a280312cda9ceec0e33cb8d60193bf878
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-02-09 16:24:51 +0100

    adding many button wall puzzle

---
+++ Assets/Project/Scripts/Interactions/Levels/C2L4/ManyButtonWallPuzzle.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ManyButtonWallPuzzle : MonoBehaviour
+{
+    [SerializeField] OpenBarsPassage _bars;
+    [SerializeField] List<GameObject> _buttonsCodeOrder = new List<GameObject>();
+    Queue<GameObject> _currentCode = new Queue<GameObject>();
+
+    private void Update()
+    {
+        if(CheckCode())
+        {
+            _bars.Interaction();
+            Destroy(this);
+        }
+    }
+
+    bool CheckCode()
+    {
+        if(_currentCode.Count == _buttonsCodeOrder.Count) 
+        {
+            for(int i = 0; i< _currentCode.Count; i++)
+            {
+                if (_currentCode.ToArray()[i] != _buttonsCodeOrder[i])
+                {
+                    return false;
+                }
+            }
+        }
+        else { return false; }
+
+        return true;
+    }
+
+    public void AddButtonToCode(GameObject button)
+    {
+        _currentCode.Enqueue(button);
+        if(_currentCode.Count > _buttonsCodeOrder.Count) 
+        {
+            _currentCode.Dequeue();
+        }
+    }
+}
